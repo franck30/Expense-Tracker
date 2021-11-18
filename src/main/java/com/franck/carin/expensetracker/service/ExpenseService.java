5dcc7e8534d38222ab32b6373a7c3067854e7b23
tchuitiocarin <tchuitiocarin@gmail.com>
2021-11-18 04:09:42 +0100

    added getExpense method in expenseService class

--- src/main/java/com/franck/carin/expensetracker/service/ExpenseService.java
+++ src/main/java/com/franck/carin/expensetracker/service/ExpenseService.java
@@ -2,6 +2,7 @@ package com.franck.carin.expensetracker.service;
 
 
 import com.franck.carin.expensetracker.dto.ExpenseDto;
+import com.franck.carin.expensetracker.exception.ExpenseNotFoundException;
 import com.franck.carin.expensetracker.model.Expense;
 import com.franck.carin.expensetracker.repository.ExpenseRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +16,49 @@ public class ExpenseService {
 
   private final ExpenseRepository expenseRepository;
 
+
+  /**
+   * method to add/create an expense
+   * @param expenseDto
+   * @return
+   */
   public String addExpense(ExpenseDto expenseDto) {
     Expense expense = mapFromDto(expenseDto);
     return expenseRepository.insert(expense).getId();
   }
+
+  /**
+   * method to retrieve/get an expense by its name
+   * @param name
+   * @return
+   */
+  public ExpenseDto getExpense(String name) {
+
+    Expense expense =  expenseRepository.findByName(name)
+            .orElseThrow(() -> new ExpenseNotFoundException(String.format("Cannot find Expense by Name - %s", name)));
+
+    return mapToDto(expense);
+
+  }
+
+
 
   private Expense mapFromDto(ExpenseDto expenseDto) {
     return Expense.builder()
             .expenseName(expenseDto.getExpenseName())
             .expenseCategory(expenseDto.getExpenseCategory())
             .expenseAmount(expenseDto.getExpenseAmount())
+            .build();
+  }
+
+
+
+  private ExpenseDto mapToDto(Expense expense) {
+    return ExpenseDto.builder()
+            .id(expense.getId())
+            .expenseName(expense.getExpenseName())
+            .expenseCategory(expense.getExpenseCategory())
+            .expenseAmount(expense.getExpenseAmount())
             .build();
   }
 
