b554ed58601c5c1a28080f348d0823f3b59b479a
tchuitiocarin <tchuitiocarin@gmail.com>
2021-11-19 01:07:17 +0100

    added method to get all expenses in service class

--- src/main/java/com/franck/carin/expensetracker/service/ExpenseService.java
+++ src/main/java/com/franck/carin/expensetracker/service/ExpenseService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -41,6 +44,16 @@ public class ExpenseService {
 
   }
 
+  /**
+   * method to get all expenses
+   * @return
+   */
+  public List<ExpenseDto> getAllExpenses() {
+
+    return expenseRepository.findAll()
+            .stream()
+            .map(this::mapToDto).collect(Collectors.toList());
+  }
 
 
   private Expense mapFromDto(ExpenseDto expenseDto) {
